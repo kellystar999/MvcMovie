8d8892ae59e4e2d209d162097b7b68b310fbf653
SOOS <PC@DESKTOP-84SIJTJ.(none)>
2024-08-15 17:27:25 +0900

    Feat:Add NuGet package

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3dbb9005dec5b94d0cdfc7bb89bff8ea38d9c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e223e8985e7e21ad720121c9603bb161dce35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
