894088ba3d688fcbf416599c087fc537571de62c
SOOS <PC@DESKTOP-84SIJTJ.(none)>
2024-08-15 16:10:46 +0900

    feat:add view .cshtml

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e99dfdc47f5fa061d9f3be7de56739f992fc671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8911fcae9d87f0c13c8149c82e7d8030c1cd2f7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
