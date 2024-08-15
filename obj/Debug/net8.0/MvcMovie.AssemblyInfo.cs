0443d13f5b73b35f76096beeb9bec82e4d448a4a
SOOS <PC@DESKTOP-84SIJTJ.(none)>
2024-08-15 15:46:38 +0900

    complete

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f474f1714fd14d6adc58ac4890e5bf82c1421036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ea34f8f919457aee9c4cf1928e1afb3983c5ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
